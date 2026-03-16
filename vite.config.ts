cb1fac406f693d83477118ed8a52c39be15ce275
sahil-chhabra-22 <scg6120@gmail.com>
2026-03-17 01:07:57 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -9,7 +10,7 @@ const __dirname = dirname(__filename);
 export default defineConfig(({ mode }) => {
   return {
     base: "/my_portfolio/",
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
